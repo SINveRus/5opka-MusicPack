09f6b7940f11500e34029d49d5961c6f71cf3666
SINveRus <117764633+SINveRus@users.noreply.github.com>
2025-04-12 20:37:59 +0500

    обновление
    доработка кода

--- music.js
+++ music.js
@@ -1,27 +1,28 @@
 const music = {
     volume: 0.7,
     menuTracks: [
-        "music/menu1.ogg",
-        "music/menu2.ogg",
-        "music/menu3.ogg"
+        "menu1.ogg",
+        "menu2.ogg",
+        "menu3.ogg"
     ],
     gameTracks: [
-        "music/game1.ogg",
-        "music/game2.ogg",
-        "music/game3.ogg",
-        "music/game4.ogg",
-        "music/game5.ogg",
-        "music/game6.ogg",
-        "music/game7.ogg",
-        "music/game8.ogg",
-        "music/game9.ogg"
+        "game1.ogg",
+        "game2.ogg",
+        "game3.ogg",
+        "game4.ogg",
+        "game5.ogg",
+        "game6.ogg",
+        "game7.ogg",
+        "game8.ogg",
+        "game9.ogg"
     ],
-    bossTrack: "music/boss1.ogg"
+    bossTrack: "game2.ogg"
 };
 
 let currentTrack = 0;
 let isPlaying = false;
 let currentMenuTrack = null;
+let isBossMusicPlaying = false;
 
 // Главная функция воспроизведения
 function playTrack(trackPath, isMusic = true) {
@@ -38,7 +39,7 @@ function playTrack(trackPath, isMusic = true) {
 
 // Циклическое воспроизведение игровых треков
 function playGameMusic() {
-    if (music.gameTracks.length === 0) return;
+    if (music.gameTracks.length === 0 || isBossMusicPlaying) return;
 
     const track = playTrack(music.gameTracks[currentTrack]);
     currentTrack = (currentTrack + 1) % music.gameTracks.length;
@@ -69,7 +70,7 @@ Events.on(EventType.MenuEnter, () => {
 });
 
 Events.on(EventType.GameStart, () => {
-    if (!isPlaying) {
+    if (!isPlaying && !isBossMusicPlaying) {
         playGameMusic();
         isPlaying = true;
     }
@@ -77,26 +78,37 @@ Events.on(EventType.GameStart, () => {
 
 // Основные события кампании
 Events.on(EventType.SectorCapture, () => {
-    if (Vars.state.isCampaign()) playGameMusic();
+    if (Vars.state.isCampaign() && !isBossMusicPlaying) playGameMusic();
 });
 
 Events.on(EventType.WaveEvent, () => {
-    if (Vars.state.isCampaign()) playGameMusic();
+    if (Vars.state.isCampaign() && !isBossMusicPlaying) playGameMusic();
 });
 
 Events.on(EventType.BossSpawn, () => {
-    playTrack(music.bossTrack);
+    if (!isBossMusicPlaying) {
+        isBossMusicPlaying = true;
+        playTrack(music.bossTrack);
+        if (isPlaying) {
+            Vars.music.stop(); // Остановка основной музыки
+        }
+    }
 });
 
 Events.on(EventType.BossDefeat, () => {
-    playGameMusic();
+    isBossMusicPlaying = false;
+    playGameMusic(); // Включаем основную музыку после смерти босса
 });
 
 // Следим за изменением состояния игры
 Events.on(EventType.StateChange, () => {
-    if (Vars.state.isCampaign() && !isPlaying) {
+    if (Vars.state.isCampaign() && !isPlaying && !isBossMusicPlaying) {
         playGameMusic();
-    } else if (Vars.state.isMenu()) {
+    } else if (Vars.state.isMenu() || Vars.state.isEditor()) {
         isPlaying = false;
+        if (isBossMusicPlaying) {
+            Vars.music.stop(); // Остановка музыки при выходе в меню или редактор
+            isBossMusicPlaying = false; // Сброс состояния босса
+        }
     }
 });
